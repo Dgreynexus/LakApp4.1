353e341e4aede3a18b6aca2daac95d468c55bfa2
Dgreynexus <nexus.ballesteros@deped.gov.ph>
2026-08-14 19:42:41 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lakapp-v4-1libas-theme';
+const CACHE='lakapp-v4-1';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
